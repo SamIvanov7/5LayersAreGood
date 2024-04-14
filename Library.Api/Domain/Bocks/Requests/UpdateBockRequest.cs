3c1448966662b861be876731f30a74b94bbec783
Sam Ivanov <si@juno-solar.com>
2024-04-14 13:37:36 +0200

    Update

--- Library.Api/Domain/Bocks/Requests/UpdateBockRequest.cs
+++ Library.Api/Domain/Bocks/Requests/UpdateBockRequest.cs
@@ -5,6 +5,7 @@
         public string Title { get; set; }
         public Guid AuthorId { get; set; }
         public string Genre { get; set; }
+        public string Description { get; set; }
 
     }
 }
